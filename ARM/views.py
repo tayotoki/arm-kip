2f355f2c1235f80dce06eb98614a809522c41f75
Constantine Putilov <putilov.constantine@yandex.ru>
2023-05-14 17:45:24 +0500

    Немного дебагов в отчете КИП, разобраться с отправкой приборов в АВЗ, пока не работает

--- ARM/views.py
+++ ARM/views.py
@@ -51,6 +51,7 @@ def create_comment(request, mech_report_id):
 
 
 def create_mech_reports(request, kip_report_id):
+    print("Пришло с запроса - kip_report_id=", kip_report_id)
     stations = {}
     other_places = Place.objects.filter(
         Q(rack__number="релейная") | Q(rack__number="тоннель") | Q(rack__number="поле"),
@@ -59,32 +60,46 @@ def create_mech_reports(request, kip_report_id):
     if request.method == "POST":
         messages.add_message(request, messages.WARNING, "Hello world.")
         kip_report = KipReport.objects.get(id=kip_report_id)
-        kip_report_devices = kip_report.devices.through.objects.all()
-        counter = 0
+        kip_report_devices = kip_report.devices.through.objects.filter(kip_report=kip_report)
+        added_devices = []
+
+        print(kip_report.id, kip_report_devices, sep='\n')
+        print("NNNNNNNNNNNNNNNN")
 
         for instance in kip_report_devices:
             other_device_on_place = [device for device in Device.objects.filter(
+                ~Q(pk=instance.device.pk),
                 station=instance.device.station,
                 mounting_address=instance.device.mounting_address
-            ) if device not in [kip_report_devices]]
+            )]
+
+            print("----------")
+            print(instance)
+            print("~~~~~~~~")
+            print(other_device_on_place)
+            print("----------")
 
             if len(other_device_on_place) > 1:
                 for current_place in other_places:
                     if instance.device.mounting_address == current_place:
                         this_place_other_device = [device for device in Device.objects.filter(
                             ~Q(status=Device.send),
+                            ~Q(pk=instance.device.pk),
                             station=instance.device.station,
+                            avz=instance.device.avz,
                             device_type=instance.device.device_type,
                             mounting_address=current_place,
-                        ) if device not in [kip_report_devices]][counter]
+                        ) if device not in added_devices][0]
 
-                        counter += 1
-                        stations.setdefault(this_place_other_device.station.pk, []).append(this_place_other_device)
+                        if this_place_other_device:
+                            added_devices.append(other_device_on_place)
+                            stations.setdefault(this_place_other_device.station.pk, []).append(this_place_other_device)
             elif len(other_device_on_place) == 1:
                 mech_device = list(other_device_on_place)[0]
                 stations.setdefault(mech_device.station.pk, []).append(mech_device)
             else:
-                ...
+                print("Отлов NoneType в station: ", instance.device)
+                stations.setdefault(instance.device.station.pk, []).append(instance.device)
             instance.device.status = Device.send
             instance.device.save()
         for station, devices in stations.items():
