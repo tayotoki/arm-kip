3c68bbaa6f2abc0a0d53d6c57080ea3ef1facad0
tayotoki <putilov.constantine@yandex.ru>
2023-05-25 16:09:27 +0500

    Сделан view для обработки кнопки Прибор замене в отчете механика для приборов с конкретным монтажным адресом

--- ARM/views.py
+++ ARM/views.py
@@ -62,30 +62,17 @@ def defect_device_action(mechanic_report_id: int,
 
 def update_device(request, device_id):
     if request.method == "POST":
-        device_id = device_id
-        device = Device.objects.get(id=device_id)
-        device.name = None
-        device.current_check_date = None
-        device.next_check_date = None
-        device.status = None
-        device.station = None
-        device.avz = None
-        device.who_prepared = None
-        device.who_checked = None
-        device.stock = Stock.objects.get(pk=1)
-        device.mounting_address = None
-        device.save(update_fields=[
-            "name",
-            "current_check_date",
-            "next_check_date",
-            "status",
-            "station",
-            "avz",
-            "who_prepared",
-            "who_checked",
-            "stock",
-            "mounting_address",
-        ])
+        exchange_device = None
+        device = Device.objects.get(pk=device_id)
+        mounting_address = device.mounting_address
+        devices_on_place = mounting_address.device_set.all()
+        for device_ in devices_on_place:
+            if device_ == device:
+                continue
+            exchange_device = device_
+        if exchange_device:
+            copy_fields(device, exchange_device)
+            send_to_stock(device.id)
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
@@ -297,11 +284,11 @@ def mark_defect_device(request, device_id):
                             return JsonResponse({"success": False,
                                                  "message": "Нет приборов, у которых можно отметить дефект"})
                     else:
-                        current_mountig_address = device.mounting_address
-                        devices = current_mountig_address.device_set
+                        current_mounting_address = device.mounting_address
+                        devices = current_mounting_address.device_set
                         if devices.count() > 2:
                             return JsonResponse({"success": False,
-                                                "message": f"На адресе {current_mountig_address}"
+                                                "message": f"На адресе {current_mounting_address}"
                                                 "больше двух приборов, проверьте"})
                         exchange_device = kip_report.devices.filter(
                             station=device.station,
